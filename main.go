3a7e9bf628d27d825b187bd2db45cfd0e842b839
hackerchai <i@hackerchai.com>
2024-02-05 15:35:14 +0800

    refactor: Use std log
    Signed-off-by: hackerchai <i@hackerchai.com>

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-
-	"log/slog"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +14,7 @@ func Cors() gin.HandlerFunc {
 		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			c.Header("Access-Control-Allow-Origin", "https://vue-proxy.zeabur.app")
+			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
@@ -31,11 +28,11 @@ func Cors() gin.HandlerFunc {
 }
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+	logger := log.Default()
 	e := gin.New()
 	e.Use(gin.Recovery())
 	e.Use(func(ctx *gin.Context) {
-		logger.Info(
+		logger.Printf(
 			"request",
 			"method", ctx.Request.Method,
 			"path", ctx.Request.URL.Path,
